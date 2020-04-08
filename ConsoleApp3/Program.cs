c7541cf63285db7c17928b598781f77c5fde21be
uzeir <uzeir@DESKTOP-7T31TSV>
2020-04-08 11:00:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+public class BaseShip 
+{
+    public BaseShip(int i)
+    {
+        Console.WriteLine("Вызван конструктор родительского класса.");
+    }
+    private int _counter;
+   
+    protected int _speed;
+    public string Move(int distance)
+    {
+        _counter++;
+
+        string result = string.Format("Пройдено километров: {0}", distance);
+        return result;
+    }
+}
+public class TransportShip : BaseShip
+{
+    public TransportShip()
+        :base(0)
+    {
+        Console.WriteLine("Вызван конструктор дочернего класса.");
+    }
+    public void start()
+    {
+
+    }
+}
+
